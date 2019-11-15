da8cdff867ec77adc231160af873d1cb30878fca
unknown <norbitrial8@gmail.com>
2019-11-15 16:17:45 +0100

    Created styled component with toggle

--- src/Components/StyledToggle/index.tsx
+++ src/Components/StyledToggle/index.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const Image = styled.div`
+    background-image: url('/assets/fruit.jpg');
+    width: 280px;
+    height: 187px;
+    margin: 0 auto;
+    border-radius: 5%;
+    box-shadow: 5px 5px 5px 0px #dbdbdb;
+`;
 
 const StyledToggle = () => {
-    return (
-        <>
-            ⛔
-        </>
-    )
+    const [visible, setVisibile] = useState(true);
+
+    return <>
+        <h3>Functional Component</h3>
+        <button onClick={() => setVisibile(!visible)}>Toggle</button>
+        <p>Visibility: <span role="img" aria-label="visibility">{visible ? '🤘' : '⛔'}</span></p>
+        {visible ? <Image /> : null}
+    </>
 }
 
 export default StyledToggle;
